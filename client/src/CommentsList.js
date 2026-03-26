7af30e01b9045533b29bb0bfccfa0e34d95fb82b
eleriin <eleriin.kurs@voco.ee>
2026-03-26 10:57:20 +0200

    Project start

---
+++ client/src/CommentsList.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const CommentsList = ({ postid }) =>{
+    const [comments, setComments] = useState([])
+
+    useEffect(()=>{
+        const fetchComments = async ()=>{
+            try{
+                const res = await axios.get(`http://localhost:5000/posts/${postid}/comments`)
+                const comments = res.data
+                setComments(comments)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchComments()
+    }, [])
+
+    const renderedComments = comments.map((comment) =>{
+        return <li key={comment.id}>{comment.content}</li>
+    })
+
+    return (
+        <ul>
+            {renderedComments}
+        </ul>
+    )
+}
+
+export default CommentsList
---
+++ client/src/PostList.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import CommentsList from './CommentsList'
+import CommentCreate from './CommentCreate'
+
+const PostList = () => {
+    const [ posts, setPosts] = useState([])
+
+    useEffect (() => {
+        const fetchPosts = async () => {
+            try{
+                const res = await axios.get('http://localhost:5000/posts')
+                const posts = res.data
+                setPosts(posts)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchPosts()
+    }, [])
+
+    console.log(posts)
+
+    const postForRender = posts.map(post =>(
+        <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
+            <div className="card-body">
+                <h3>{post.title}</h3>
+                <CommentsList postid={post.id} />
+                <CommentCreate postid={post.id} />
+            </div>
+        </div>
+    ));
+
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {postsForRender}
+        </div>
+    )
+}
+
+export default PostList;
